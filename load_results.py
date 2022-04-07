6cf89014c17cd05c600f3cf801457549588c1b3d
jyli <lijinyang161@mails.ucas.ac.cn>
2022-04-07 14:39:01 +0800

    mgf generator and writer

--- load_results.py
+++ load_results.py
@@ -3,17 +3,18 @@ import pandas as pd
 
 def get_seq_mod_from_pF_res(res_path):
     '''
-    Load columns from pFind results
+    Load columns from .spectra text file from pFind results as pd.DataFrame
     '''
     
-    spectra_file = pd.read_csv(res_path, delimiter='\t')
-    spectra_file = spectra_file[['File_Name', 'Sequence', 'Modification', 'Proteins']]
     '''
+    The columns are like:
     File_Name	Scan_No	Exp.MH+	Charge	Q-value	Sequence	Calc.MH+	Mass_Shift(Exp.-Calc.)
     Raw_Score	Final_Score	Modification	Specificity	Proteins	Positions	Label
     Target/Decoy	Miss.Clv.Sites	Avg.Frag.Mass.Shift	Others
     '''
-
+    
+    spectra_file = pd.read_csv(res_path, delimiter='\t')
+    spectra_file = spectra_file[['File_Name', 'Sequence', 'Modification', 'Proteins']]
     spectra_file = spectra_file.rename(columns={
         "File_Name": "title", 
         "Sequence": "sequence", 
@@ -23,20 +24,20 @@ def get_seq_mod_from_pF_res(res_path):
     return spectra_file
 
 
-def get_seq_mod_from_pL_res(res_path): # title, sequence, modinfo
+def get_seq_mod_from_pL_res(res_path):
     '''
-    Load columns from pLink results
-    
+    Load columns from _spectra.csv text file from pLink results as pd.DataFrame
     '''
-    spectra_file = pd.read_csv(res_path)
-    spectra_file = spectra_file[['Title', 'Linker', 'Peptide', 'Modifications', 'Proteins']].fillna("")
+
     """
+    The columns are like:
     Order, Title, Charge, Precursor_Mass, Peptide, Peptide_Type, Linker, Peptide_Mass, Modifications,
     Evalue, Score, Precursor_Mass_Error(Da), Precursor_Mass_Error(ppm), Proteins, Protein_Type, 
     FileID, LabelID, Alpha_Matched, Beta_Matched, Alpha_Evalue, Beta_Evalue
     """
 
-    spectra_file['Linker'].replace(['Leiker_clv'], 'Leiker', inplace=True)
+    spectra_file = pd.read_csv(res_path)
+    spectra_file = spectra_file[['Title', 'Linker', 'Peptide', 'Modifications', 'Proteins']].fillna("")
     spectra_file = spectra_file.rename(columns={
         "Title": "title",
         "Peptide": "sequence",
