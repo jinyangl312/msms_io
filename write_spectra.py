228f15eec4cd426e40483b02f0e25d100ce734ec
jyli <lijinyang161@mails.ucas.ac.cn>
2023-05-21 16:07:50 +0800

    transform mgf to pf2

--- write_spectra.py
+++ write_spectra.py
@@ -3,12 +3,74 @@ from pathlib import Path
 import tqdm
 from theoretical_peaks.AAMass import aamass
 import re
+import struct
+from .load_spectra import mgf_loader_unit
+import pathlib
 
 
-# def write_pf2_header(path, nSpec, lenTitle, pf2title):
-#     with open(path, "wb") as f:
-#         f.write(struct.pack("2i", nSpec, lenTitle))
-#         f.write(struct.pack("%ds" % lenTitle, pf2title))
+def transform_mgf_to_pf2(mgf_dir):
+    if not isinstance(mgf_dir, list):
+        mgf_dir = Path(mgf_dir)
+        assert mgf_dir.is_dir()
+        mgf_list = mgf_dir.glob('*.mgf')
+
+    for mgf_path in tqdm.tqdm(mgf_list):
+        transform_mgf_to_pf2_unit(str(mgf_path))
+
+
+def transform_mgf_to_pf2_unit(mgf_path):
+    assert '_' in pathlib.Path(mgf_path).stem
+    pf2idx_path = mgf_path.replace('.mgf', '.pf2idx')
+    pf2_path = mgf_path.replace('.mgf', '.pf2')
+    pf2title = pathlib.Path('_'.join(mgf_path.split('_')[:-1])).stem
+
+    mgf_loader = mgf_loader_unit(mgf_path, transform_peaks=True)
+
+    # Group mgf by scan_id, merge mixed spectra
+    mgf_scan_dict = dict()
+    mgf_scan_no_list = list()
+    for spec_info, peaks in tqdm.tqdm(mgf_loader):
+        scan_no = int(re.search("\d+(?=\.\d+\.\d+\.\d+\.dta)",
+                      spec_info['TITLE']).group())
+        if scan_no not in mgf_scan_dict:
+            mgf_scan_dict[scan_no] = [(
+                scan_no, len(peaks),
+                [y for x in peaks for y in x]
+            )]
+            mgf_scan_no_list.append(scan_no)
+        mgf_scan_dict[scan_no].append((
+            float(spec_info['PEPMASS']),
+            int(re.search("\d+", spec_info['CHARGE']).group())
+        ))
+
+    # Write pf2 and pf2idx
+    pf2idx_file = open(pf2idx_path, 'wb')
+    pf2_file = open(pf2_path, 'wb')
+
+    offset = 0
+    pf2_file.write(struct.pack("2i", len(mgf_scan_no_list), len(pf2title)))
+    pf2_file.write(struct.pack("%ds" %
+                   len(pf2title), bytes(pf2title, 'utf-8')))
+    offset += 2*4 + len(pf2title)*1
+
+    for scan_no in tqdm.tqdm(mgf_scan_no_list):
+        pf2idx_file.write(struct.pack("i", scan_no))  # scan_no
+        pf2idx_file.write(struct.pack("i", offset))  # offset
+
+        scan_info = mgf_scan_dict[scan_no]
+        pf2_file.write(struct.pack("i", scan_info[0][0]))  # scan_no
+        pf2_file.write(struct.pack("i", scan_info[0][1]))  # nPeak
+        pf2_file.write(struct.pack(
+            str(scan_info[0][1]*2)+"d", *scan_info[0][2]))  # peaks
+        pf2_file.write(struct.pack("i", len(scan_info)-1))  # nMix
+        offset += 3*4 + scan_info[0][1]*2*8
+        for i in range(1, len(scan_info)):
+            pf2_file.write(struct.pack("d", scan_info[i][0]))  # precursor
+            pf2_file.write(struct.pack("i", scan_info[i][1]))  # nCharge
+        offset += (len(scan_info)-1) * (4+8)
+
+    pf2idx_file.close()
+    pf2_file.close()
 
 
 def write_mgf(writer, header, peaks):
